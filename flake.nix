3bdbdd64fb541e4b6ce4c2791cfb3554b83610e7
qwerty <temp@qwerty.qq>
2025-08-01 14:35:25 +0300

    fix

--- flake.nix
+++ flake.nix
@@ -13,7 +13,10 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }:
+  # <<< ИЗМЕНЕНИЕ ЗДЕСЬ >>>
+  # Мы добавляем home-manager и hyprland в список переменных,
+  # которые извлекаются из inputs.
+  outputs = { self, nixpkgs, home-manager, hyprland, ... }@inputs:
     let
       stateVersion = "25.05";
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
@@ -27,10 +30,12 @@
       nixosConfigurations = {
         qwerty = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
+          # Мы по-прежнему передаем ВЕСЬ набор inputs в модули, это правильно.
           specialArgs = { inherit inputs stateVersion; };
           modules = [
             ./nixos/configuration.nix
 
+            # Теперь эта строка будет работать, т.к. home-manager в области видимости.
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
