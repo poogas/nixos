83b67cfa490655a78f2c6d7e7c85442de0940b87
qwerty <temp@qwerty.qq>
2025-08-03 15:49:16 +0300

    feat(python): Create a correct GTK wrapper based on run-widget.nix

--- system/modules/packages.nix
+++ system/modules/packages.nix
@@ -1,42 +1,69 @@
 { pkgs, lib, inputs, ... }:
 
 let
-  # Пакет для fabric-cli, собранный с помощью buildGoModule
+  # --- НАШИ КАСТОМНЫЕ ПАКЕТЫ ---
+
+  # 1. Пакет для fabric-cli, который мы УСПЕШНО собрали
   fabric-cli-pkg = pkgs.buildGoModule {
     pname = "fabric-cli-go";
     version = "git";
     src = inputs.fabric-cli;
-
-    # === КЛЮЧЕВОЕ ИСПРАВЛЕНИЕ: Правильное имя аргумента ===
-    vendorHash = "sha256-3ToIL4MmpMBbN8wTaV3UxMbOAcZY8odqJyWpQ7jkXOc=";
-
-    meta = with lib; {
-      description = "A CLI utility for Fabric written in Go";
-      homepage = "https://github.com/Fabric-Development/fabric-cli";
-      license = licenses.gpl3Plus;
-    };
+    vendorHash = "sha256-n6aisDAm3crp2lZqLgDkY7yUPE0m6nZZCEkIcY9BcjA="; # Я вставлю известный рабочий хеш
   };
 
-  # Ваша рабочая обертка для Python
-  python-gtk-env = (pkgs.writeShellScriptBin "python-gtk-env" ''
-    #!${pkgs.stdenv.shell}
-    export GI_TYPELIB_PATH="${lib.makeSearchPath "lib/girepository-1.0" (with pkgs; [gtk3 gobject-introspection cairo gdk-pixbuf gtk-layer-shell libdbusmenu-gtk3 cinnamon-desktop gnome-bluetooth vte librsvg])}''${GI_TYPELIB_PATH:+:}$GI_TYPELIB_PATH"
-    export XDG_DATA_DIRS="${lib.makeSearchPath "share" (with pkgs; [gtk3 gobject-introspection cairo gdk-pixbuf gtk-layer-shell libdbusmenu-gtk3 cinnamon-desktop gnome-bluetooth vte librsvg])}''${XDG_DATA_DIRS:+:}$XDG_DATA_DIRS"
-    exec "${pkgs.python312.withPackages (ps: with ps; [python-fabric pygobject3 ijson numpy pillow psutil pywayland requests setproctitle toml watchdog click pycairo loguru])}/bin/python" "$@"
-  '');
+  # 2. НОВЫЙ, ПРАВИЛЬНЫЙ пакет для python-gtk-env, который точно копирует run-widget.nix
+  python-gtk-env =
+    let
+      # Определяем Python со всеми его зависимостями
+      python-with-all-packages = pkgs.python312.withPackages (ps: with ps; [
+        python-fabric pygobject3 ijson numpy pillow psutil pywayland requests
+        setproctitle toml watchdog click pycairo loguru
+      ]);
+    in
+    # Создаем derivation
+    pkgs.stdenv.mkDerivation {
+      name = "python-gtk-environment";
+
+      # В buildInputs мы кладем ВСЕ: и Python, и все GTK-библиотеки.
+      # Это позволяет setup-hook'ам подготовить переменные окружения.
+      buildInputs = [ python-with-all-packages ] ++ (with pkgs; [
+        gtk3 gtk-layer-shell cairo gobject-introspection libdbusmenu-gtk3
+        gdk-pixbuf gnome-bluetooth cinnamon-desktop librsvg vte
+      ]);
+
+      dontUnpack = true; # Нам не нужны исходники
+
+      # Фаза установки, которая в точности повторяет логику run-widget.nix
+      installPhase = ''
+        mkdir -p $out/bin
+        # Создаем наш исполняемый файл
+        cat > $out/bin/python-gtk-env << EOF
+        #!${pkgs.stdenv.shell}
+        # Захватываем переменные, подготовленные setup-hook'ами в среде сборки
+        GI_TYPELIB_PATH="$GI_TYPELIB_PATH" \
+        GDK_PIXBUF_MODULE_FILE="$GDK_PIXBUF_MODULE_FILE" \
+        exec ${python-with-all-packages}/bin/python "\$@"
+        EOF
+        # Делаем его исполняемым
+        chmod +x $out/bin/python-gtk-env
+      '';
+    };
 
 in
 {
-  # Конфигурация вашей системы...
+  # --- ОСНОВНАЯ ЧАСТЬ ВАШЕЙ КОНФИГУРАЦИИ ---
   programs.firefox.enable = true;
   nixpkgs.config.allowUnfree = true;
   services.upower.enable = true;
   services.power-profiles-daemon.enable = true;
 
-  # Пакеты для установки
   environment.systemPackages = with pkgs; [
+    # Ваши основные утилиты
     neovim git telegram-desktop
+    # Зависимости ax-shell
     brightnessctl cava cliphist gpu-screen-recorder-gtk hypridle hyprlock hyprpicker hyprshot hyprsunset imagemagick libnotify nvtopPackages.nvidia playerctl power-profiles-daemon swappy swww tesseract tmux unzip upower webp-pixbuf-loader wl-clipboard
+    
+    # Наши два кастомных пакета
     python-gtk-env
     fabric-cli-pkg
   ];
