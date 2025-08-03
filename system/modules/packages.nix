772856c7876908eeb6739d2157138a47ae505574
qwerty <temp@qwerty.qq>
2025-08-03 14:13:09 +0300

    feat: Implement final python wrapper script

--- system/modules/packages.nix
+++ system/modules/packages.nix
@@ -2,6 +2,7 @@
 
 let
   # Шаг 1: Определяем все системные C-библиотеки, которые нам нужны.
+  # Этот список мы взяли из default.nix самого Fabric, чтобы обеспечить совместимость.
   gtk-dependencies = with pkgs; [
     gtk3
     gobject-introspection
@@ -14,7 +15,7 @@ let
     vte
   ];
 
-  # Шаг 2: Создаем интерпретатор Python со всеми нужными Python-библиотеками.
+  # Шаг 2: Создаем интерпретатор Python со всеми нужными ему Python-библиотеками.
   python-with-fabric = pkgs.python312.withPackages (ps: with ps; [
     python-fabric
     pygobject3
@@ -46,7 +47,7 @@ in
     git
     telegram-desktop
 
-    # Утилиты для ax-shell
+    # Системные утилиты, необходимые для ax-shell
     brightnessctl
     cava
     cliphist
@@ -70,17 +71,23 @@ in
     webp-pixbuf-loader
     wl-clipboard
 
-    # ======================== ФИНАЛЬНОЕ РЕШЕНИЕ (v2) =======================
+    # ======================== ФИНАЛЬНОЕ РЕШЕНИЕ ========================
     # Шаг 3: Создаем скрипт-обертку.
-    # Мы используем writeShellScriptBin и конструируем тело скрипта с помощью
-    # двойных кавычек, что позволяет Nix правильно вычислить выражения `${...}`.
+    # Мы используем `writeShellScriptBin` и двойные кавычки ("...") для правильной
+    # подстановки Nix-переменных в итоговый shell-скрипт.
     (pkgs.writeShellScriptBin "python-with-ax-shell-env" ''
-      # Nix вычислит эти функции и подставит сюда результат (пути)
-      export GI_TYPELIB_PATH="${pkgs.lib.makeSearchPathOutput "lib/girepository-1.0" gtk-dependencies}:${"$"}{GI_TYPELIB_PATH:-}"
-      export XDG_DATA_DIRS="${pkgs.lib.makeSearchPath "share" gtk-dependencies}:${"$"}{XDG_DATA_DIRS:-}"
+      #!${pkgs.stdenv.shell}
 
-      # Запускаем наш Python, передавая ему все аргументы ("$@")
-      exec ${python-with-fabric}/bin/python "$@"
+      # Устанавливаем переменную для поиска .typelib файлов (карты C-библиотек).
+      # Мы добавляем пути к существующей переменной, если она уже задана.
+      export GI_TYPELIB_PATH="${pkgs.lib.makeSearchPathOutput "lib/girepository-1.0" gtk-dependencies}''${GI_TYPELIB_PATH:+:}$GI_TYPELIB_PATH"
+
+      # Устанавливаем переменную для поиска данных (иконки, схемы и т.д.)
+      export XDG_DATA_DIRS="${pkgs.lib.makeSearchPath "share" gtk-dependencies}''${XDG_DATA_DIRS:+:}$XDG_DATA_DIRS"
+
+      # Запускаем наш специально собранный Python, передавая ему все аргументы.
+      # `exec` заменяет процесс оболочки процессом Python для эффективности.
+      exec "${python-with-fabric}/bin/python" "$@"
     '')
     # =======================================================================
   ];
